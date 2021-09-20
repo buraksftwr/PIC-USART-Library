53d21ec763d78a7fb58b3bc93febf99384500b89
Burak İyikesici <burakiyikesici@gmail.com>
2021-09-20 20:44:43 +0300

    Uploaded for the first  time.

--- USARTLib.c
+++ USARTLib.c
@@ -5,22 +5,22 @@
 
 void USARTInit(uint16_t baudrate, uint8_t mode){
 	if(baudrate == 1200) // SPBRG bit is configured for 4 MHz crystal oscillator
-	        SPBRG = 207;               
-	    else if(baudrate == 2400)
-	        SPBRG = 103;                
-	    else if(baudrate == 9600)
-	        SPBRG = 25;               
-	    else if(baudrate == 19200)
-	        SPBRG = 12;               
-	    else if(baudrate == 28800)
-	        SPBRG = 8;               
-	    else if(baudrate == 33600)
-	        SPBRG = 6;
-	    else
-	        SPBRG = 0; 
+	    SPBRG = 207;               
+	else if(baudrate == 2400)
+	    SPBRG = 103;                
+	else if(baudrate == 9600)
+	    SPBRG = 25;               
+	else if(baudrate == 19200)
+	    SPBRG = 12;               
+	else if(baudrate == 28800)
+	    SPBRG = 8;               
+	else if(baudrate == 33600)
+	    SPBRG = 6;
+	else
+	    SPBRG = 0; 
 	        
-    switch(mode){	// The code is configured to operate in asynchronous high speed mode
-        case 0: // Both transmit and receive mode is selected
+    switch(mode){ // The code is configured to operate in asynchronous high-speed mode
+        case 0: // Both transmit and receive modes are selected
             TXSTA = 0x24;
             RCSTA = 0x90;
             break;
